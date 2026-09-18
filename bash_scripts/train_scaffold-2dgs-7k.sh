2da16f23023a80ddd1a072f041f195000ffe9040
yanxian <yanxiancsu@csu.edu.cn>
2026-09-18 20:29:13 +0800

    Make 7k Scaffold-2DGS wrapper permission-safe

--- bash_scripts/train_scaffold-2dgs-7k.sh
+++ bash_scripts/train_scaffold-2dgs-7k.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 export ITERATIONS="${ITERATIONS:-7000}"
 export RUN_NAME="${RUN_NAME:-pipeline-7k}"
 
-exec "$SCRIPT_DIR/train_scaffold-2dgs.sh" "$@"
+exec bash "$SCRIPT_DIR/train_scaffold-2dgs.sh" "$@"
